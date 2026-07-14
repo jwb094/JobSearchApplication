e51a6cf89be8ac1e7e6002f6d68dc22bbecfc60c
Josh Bello <Jb230913@live.com>
2026-07-14 22:05:51 +0100

    add - new data file

---
+++ src/data/filter.js
@@ -0,0 +1,28 @@
+export const jobTypes = [
+  { id: "full-time", label: "Full-Time" },
+  { id: "part-time", label: "Part-Time" },
+  { id: "contract", label: "Contract" },
+  { id: "remote", label: "Remote" },
+];
+
+export const experience = [
+  { id: "entry-level", label: "Entry-Level / Associate" },
+  { id: "junior", label: "Junior" },
+  { id: "mid-level", label: "Mid-Level" },
+  { id: "senior", label: "Senior" },
+];
+
+export const locations = [
+  { id: "london", label: "London" },
+  { id: "liverpool", label: "Liverpool" },
+  { id: "newcastle", label: "Newcastle" },
+  { id: "bristol", label: "Bristol" },
+];
+
+export const salaries = [
+  { id: "20k-30k", label: "£20,000–£30,000" },
+  { id: "30k-40k", label: "£30,000–£40,000" },
+  { id: "40k-50k", label: "£40,000–£50,000" },
+  { id: "50k-60k", label: "£50,000–£60,000" },
+  { id: "60k-plus", label: "£60,000+" },
+];
