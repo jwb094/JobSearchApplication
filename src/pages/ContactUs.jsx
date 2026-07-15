94b1b9cdf5b744cd2df01feeb028a78e08286ad6
Josh Bello <Jb230913@live.com>
2026-07-15 22:18:47 +0100

    update - added component and html content

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import Form from "../components/Form"
+import React from "react";
+import Form from "../components/Form";
 function ContactUs(props) {
-    return (
-        <>
-                  <div className="flex flex-col gap-y-6 | mx-auto px-4 py-16 sm:px-6 lg:px-12 |  max-w-7xl">
+  return (
+    <>
+      <div className="flex flex-col gap-y-6 | mx-auto px-4 py-16 sm:px-6 lg:px-12 |  max-w-7xl">
         <h2 className="text-3xl">Contact Us</h2>
 
         <b>We'd Love to Hear From You</b>
         <p>
-  Whether you have a question about the platform, need help with your account, want to report an issue, or have feedback on how we can improve your job search experience, our team is here to help.
-
-Simply complete the contact form below, and we'll respond as soon as possible.
+          Whether you have a question about the platform, need help with your
+          account, want to report an issue, or have feedback on how we can
+          improve your job search experience, our team is here to help. Simply
+          complete the contact form below, and we'll respond as soon as
+          possible.
         </p>
       </div>
 
-        <Form/>
-
-        </>
-    );
+      <Form />
+    </>
+  );
 }
 
 export default ContactUs;
