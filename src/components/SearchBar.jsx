cb53ca4d5da340fe2a17529d3ba56641921b9e36
Josh Bello <Jb230913@live.com>
2026-07-16 11:11:32 +0100

    update added utility class for ml

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -28,7 +28,7 @@ function SearchBar(props) {
           />
 
           <button
-            className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+            className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none md:ml-2"
             type="button"
           >
             Search
