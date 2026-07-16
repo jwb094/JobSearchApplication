3c8775913a19547c717bb66c04778aa08294a9f3
Josh Bello <Jb230913@live.com>
2026-07-16 10:36:13 +0100

    update - added new route for catgeory page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import  React  from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 
 import "./App.css";
 import Default from "./components/layouts/Default";
@@ -9,18 +9,20 @@ import Job from "./pages/Job";
 import SearchPage from "./pages/SearchPage";
 import PageNotFound from "./pages/PageNotFound";
 import About from "./pages/About";
+import CategoryPage from "./pages/CategoryPage";
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Default/>}>
-        <Route index element={<Home />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="/job/:id/:slug" element={<Job />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/about" element={<About />} /> 
-        <Route path="*" element={<PageNotFound />} /> 
-         </Route>
+      <Routes>
+        <Route path="/" element={<Default />}>
+          <Route index element={<Home />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/job/category/:category_slug" element={<CategoryPage />} />
+          <Route path="/job/:id/:slug" element={<Job />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
       </Routes>
     </>
   );
