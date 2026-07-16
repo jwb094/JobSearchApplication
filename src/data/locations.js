ea34e315c0af5437ebfab9238c99dec9180d7508
Josh Bello <Jb230913@live.com>
2026-07-16 11:17:27 +0100

    add - new dataset

---
+++ src/data/locations.js
@@ -0,0 +1,256 @@
+export const locations = [
+
+{
+    id: 1,
+    city: "London",
+    area: "Central London",
+    postcode: "WC1V 6EA",
+    region: "Greater London",
+    country: "United Kingdom"
+},
+
+{
+    id: 2,
+    city: "London",
+    area: "Shoreditch",
+    postcode: "EC2A 4NE",
+    region: "Greater London",
+    country: "United Kingdom"
+},
+
+{
+    id: 3,
+    city: "London",
+    area: "Soho",
+    postcode: "W1D 3QF",
+    region: "Greater London",
+    country: "United Kingdom"
+},
+
+{
+    id: 4,
+    city: "London",
+    area: "Westminster",
+    postcode: "SW1A 2AA",
+    region: "Greater London",
+    country: "United Kingdom"
+},
+
+{
+    id: 5,
+    city: "London",
+    area: "Canary Wharf",
+    postcode: "E14 5AB",
+    region: "Greater London",
+    country: "United Kingdom"
+},
+
+{
+    id: 6,
+    city: "London",
+    area: "Paddington",
+    postcode: "W2 1HQ",
+    region: "Greater London",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 7,
+    city: "Manchester",
+    area: "City Centre",
+    postcode: "M1 1AE",
+    region: "Greater Manchester",
+    country: "United Kingdom"
+},
+
+{
+    id: 8,
+    city: "Manchester",
+    area: "Salford Quays",
+    postcode: "M50 3SP",
+    region: "Greater Manchester",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 9,
+    city: "Birmingham",
+    area: "City Centre",
+    postcode: "B1 1TB",
+    region: "West Midlands",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 10,
+    city: "Reading",
+    area: "Town Centre",
+    postcode: "RG1 1LZ",
+    region: "Berkshire",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 11,
+    city: "Brighton",
+    area: "Brighton Centre",
+    postcode: "BN1 1EE",
+    region: "East Sussex",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 12,
+    city: "Croydon",
+    area: "Central Croydon",
+    postcode: "CR0 1EA",
+    region: "Greater London",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 13,
+    city: "Leeds",
+    area: "City Centre",
+    postcode: "LS1 4AP",
+    region: "West Yorkshire",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 14,
+    city: "Cambridge",
+    area: "Science Park",
+    postcode: "CB4 0WS",
+    region: "Cambridgeshire",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 15,
+    city: "Bristol",
+    area: "Temple Quarter",
+    postcode: "BS1 6QF",
+    region: "South West England",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 16,
+    city: "Guildford",
+    area: "Town Centre",
+    postcode: "GU1 1AA",
+    region: "Surrey",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 17,
+    city: "Nottingham",
+    area: "City Centre",
+    postcode: "NG1 2AS",
+    region: "Nottinghamshire",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 18,
+    city: "Sheffield",
+    area: "City Centre",
+    postcode: "S1 2BJ",
+    region: "South Yorkshire",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 19,
+    city: "Oxford",
+    area: "City Centre",
+    postcode: "OX1 1HS",
+    region: "Oxfordshire",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 20,
+    city: "Liverpool",
+    area: "City Centre",
+    postcode: "L1 1JD",
+    region: "Merseyside",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 21,
+    city: "Newcastle",
+    area: "City Centre",
+    postcode: "NE1 4LP",
+    region: "Tyne and Wear",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 22,
+    city: "Leicester",
+    area: "City Centre",
+    postcode: "LE1 5YA",
+    region: "Leicestershire",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 23,
+    city: "Southampton",
+    area: "City Centre",
+    postcode: "SO14 7DW",
+    region: "Hampshire",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 24,
+    city: "Milton Keynes",
+    area: "Central Milton Keynes",
+    postcode: "MK9 3EP",
+    region: "Buckinghamshire",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 25,
+    city: "Remote",
+    area: "UK Remote",
+    postcode: null,
+    region: "United Kingdom",
+    country: "United Kingdom"
+},
+
+
+{
+    id: 26,
+    city: "Remote",
+    area: "Remote Europe",
+    postcode: null,
+    region: "Europe",
+    country: "Remote"
+}
+
+];
