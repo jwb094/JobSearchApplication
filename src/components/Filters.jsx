e8017ad0367a51d40dc82e9774f47bbd7db8f055
Josh Bello <Jb230913@live.com>
2026-07-16 11:11:18 +0100

    update - added if statement tot display icons and added reset filter button

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import FilterSection from "./Filters/FilterSection";
 import { jobTypes, experience, locations, salaries } from "../data/filter";
+import { HambugerIcon,ClosedIcon } from "../assets/icons";
 function Filters(props) {
   const [isOpen, setIsOpen] = useState(false);
   return (
@@ -9,8 +10,10 @@ function Filters(props) {
       data-view="default"
       className="c-filters | px-4 py-6  bg-teal-400"
     >
-      <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
-        ☰ Filters
+      <button className="flex flex-row gap-6 md:hidden" onClick={() => setIsOpen(!isOpen)}>
+      
+        {!isOpen ? <HambugerIcon width={24} height={24}/> : <ClosedIcon width={24} height={24}/>}
+        Filters
       </button>
       <div className={isOpen ? "block" : "hidden md:block"}>
         <FilterSection title={"Job Type"} options={jobTypes} />
@@ -20,6 +23,13 @@ function Filters(props) {
         <FilterSection title={"Locations"} options={locations} />
 
         <FilterSection title={"Salaries"} options={salaries} />
+
+        <button
+          class="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+          type="button"
+        >
+          Reset Filters
+        </button>
       </div>
     </section>
   );
