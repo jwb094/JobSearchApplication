2303e6360088c0c40347f523d8aae0bd325563d4
Josh Bello <Jb230913@live.com>
2026-07-18 15:25:08 +0100

    update - Add ContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import App from "./App.jsx";
+import { FiltersProvider } from "./context/Filters.jsx";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <BrowserRouter>
-      <App />
+    <BrowserRouter>
+      <FiltersProvider>
+        <App />
+      </FiltersProvider>
     </BrowserRouter>
   </StrictMode>,
-)
+);
