bbad882392badff10aed78b195ce8a176766ada5
Josh Bello <Jb230913@live.com>
2026-07-18 15:23:25 +0100

    update - add sortList via prop to <select>

--- src/components/SortDropdown.jsx
+++ src/components/SortDropdown.jsx
@@ -9,11 +9,13 @@ function SortDropdown(props) {
     >
       <div className="md:flex md:justify-end | w-full">
         <div className="relative">
-          <select className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer">
-            <option value="brazil">Published - Desc</option>
-            <option value="bucharest">Published - ASc</option>
-            <option value="london">Alp - A- Z</option>
-            <option value="washington">Alp - Z - A</option>
+          <select  
+          onChange={props.sortList}
+          className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer">
+            <option value="publish-desc">Published - Desc</option>
+            <option value="publish-asc">Published - ASc</option>
+            <option value="A-Z">Alp - A- Z</option>
+            <option value="Z-A">Alp - Z - A</option>
           </select>
           <svg
             xmlns="http://www.w3.org/2000/svg"
