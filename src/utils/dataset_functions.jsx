ec08ac9218297c4c062a04235beb8a65acdb1e0e
Josh Bello <Jb230913@live.com>
2026-07-20 16:05:54 +0100

    update - addedd logic to getFeaturedCategoryJobs getJobsByCatgeory getCategoryBySlug filterJobs checkJobDescriptionSlug getJobDescByID getJobsFromSameCategory

--- src/utils/dataset_functions.jsx
+++ src/utils/dataset_functions.jsx
@@ -11,7 +11,9 @@ let mostRecentFearturedJobs = jobs
   .map((job) => {
     let more_info = getDetails(job);
     return {
+      id: job.id,
       company_name: more_info.company_name,
+      jobType: more_info.jobType,
       job_type: more_info.job_type,
       salary_min: more_info.salary_min,
       salary_max: more_info.salary_max,
@@ -42,6 +44,68 @@ export const getRandomCategories = [...categories]
     };
   });
 
+export const getFeaturedCategoryJobs = (categoryId) => {
+  const jobCategoryResults = jobs
+    .filter((job) => job.category_id === categoryId)
+    .filter((job) => job.featured)
+    .map((job) => {
+      let more_info = getDetails(job);
+      return {
+        id: job.id,
+        company_name: more_info.company_name,
+        jobType: more_info.jobType,
+        job_type: job.job_type,
+        salary_min: more_info.salary_min,
+        salary_max: more_info.salary_max,
+        url: `/job/${job.id}/${job.slug}`,
+        featured: job.featured,
+        category_id: job.category_id,
+        title: job.title,
+        job_location: job.location ? job.location : more_info.job_location,
+        date_posted: job.posted_date,
+        experience: more_info.experience,
+      };
+    })
+    .slice(0, 3);
+
+  return jobCategoryResults;
+};
+
+export const getJobsByCatgeory = (category) => {
+  let selectedCat = categories.find((cat) => cat.slug === category);
+
+  let featuredCategoryJobIDs = getFeaturedCategoryJobs(selectedCat.id);
+
+  const jobCategoryResults = jobs
+    .filter((job) => job.category_id === selectedCat.id)
+    .filter(
+      (job) =>
+        !featuredCategoryJobIDs.some((featured) => featured.id === job.id),
+    )
+    .map((job) => {
+      let more_info = getDetails(job);
+      return {
+        id: job.id,
+        company_name: more_info.company_name,
+        jobType: more_info.jobType,
+        job_type: job.job_type,
+        salary_min: more_info.salary_min,
+        salary_max: more_info.salary_max,
+        url: `/job/${job.id}/${job.slug}`,
+        title: job.title,
+        job_location: job.location ? job.location : more_info.job_location,
+        date_posted: job.posted_date,
+        experience: more_info.experience,
+      };
+    });
+
+  return jobCategoryResults;
+};
+
+export const getCategoryBySlug = (categorySlug) => {
+  return categories.find((cat) => cat.slug === categorySlug);
+};
+
 export const SearchJobDB = (search_term, filters) => {
   if (!search_term.trim()) return jobs;
 
@@ -53,6 +117,7 @@ export const SearchJobDB = (search_term, filters) => {
       let more_info = getDetails(job);
       let jobLocation = job.location ? job.location : more_info.job_location;
       return {
+        id: job.id,
         company_name: more_info.company_name,
         jobType: more_info.jobType,
         job_type: job.job_type,
@@ -90,16 +155,83 @@ function getDetails(job) {
   };
 }
 
-export const getJobs = function (page, limit, data, sort, filters) {
-  let sortedData = [...data];
+// export const getJobs = function (page, limit, data, sort, filters) {
+//   let sortedData = [...data];
+//   console.log(filters);
+//   sortedData = sortedData.filter((job) => {
+//     const matchesJobType =
+//       filters.jobType.length === 0 || filters.jobType.includes(job.job_type);
 
-  sortedData = sortedData.filter((job) => {
+//     const matchesSalaries =
+//       filters.salaries.length === 0 ||
+//       filters.salaries.some(
+//         (salary) => Number(job.salary_min.replace(",", "")) >= salary,
+//       );
+
+//     const matchesLocationType =
+//       filters.location.length === 0 ||
+//       filters.location.includes(job.job_location);
+
+//     const matchesExperienceType =
+//       filters.experience.length === 0 ||
+//       filters.experience.includes(job.experience);
+
+//     return (
+//       matchesJobType &&
+//       matchesLocationType &&
+//       matchesExperienceType &&
+//       matchesSalaries
+//     );
+//   });
+//    console.log(sortedData);
+//   // let totalAmount = sortedData.length;
+//   switch (sort) {
+//     case "oldest":
+//       sortedData.sort((a, b) =>
+//         new Date(b.posted_date) - new Date(a.posted_date) ? 1 : -1,
+//       );
+//       break;
+
+//     case "newest":
+//       sortedData.sort(
+//         (a, b) => new Date(a.posted_date) - new Date(b.posted_date),
+//       );
+//       break;
+
+//     case "az":
+//       sortedData.sort((a, b) => a.title.localeCompare(b.title));
+//       break;
+
+//     case "za":
+//       sortedData.sort((a, b) => b.title.localeCompare(a.title));
+//       break;
+//     default:
+//       break;
+//   }
+
+//  return sortedData.slice((page - 1) * limit, page * limit);
+//   // return {
+//   //   "total":totalAmount,
+//   //   "jobToDisplay":sortedData.slice((page - 1) * limit, page * limit)};
+//  /**
+//    *
+//    * Page 1 = 0-9
+//    * Page 2 = 9-18
+//    * Page 3 = 18-27
+//    */
+// };
+
+export const filterJobs = function (data, filters) {
+  let sortedData = [...data];
+  return sortedData.filter((job) => {
     const matchesJobType =
       filters.jobType.length === 0 || filters.jobType.includes(job.job_type);
 
     const matchesSalaries =
       filters.salaries.length === 0 ||
-      filters.salaries.some((salary) => Number(job.salary_min.replace(",","")) >= salary);
+      filters.salaries.some(
+        (salary) => Number(job.salary_min.replace(",", "")) >= salary,
+      );
 
     const matchesLocationType =
       filters.location.length === 0 ||
@@ -109,9 +241,19 @@ export const getJobs = function (page, limit, data, sort, filters) {
       filters.experience.length === 0 ||
       filters.experience.includes(job.experience);
 
-
-    return matchesJobType && matchesLocationType && matchesExperienceType && matchesSalaries;
+    return (
+      matchesJobType &&
+      matchesLocationType &&
+      matchesExperienceType &&
+      matchesSalaries
+    );
   });
+
+  //return sortedData;
+};
+
+export const sortJobs = function (data, sort) {
+  let sortedData = [...data];
 
   switch (sort) {
     case "oldest":
@@ -137,13 +279,7 @@ export const getJobs = function (page, limit, data, sort, filters) {
       break;
   }
 
-  return sortedData.slice((page - 1) * limit, page * limit);
-  /**
-   *
-   * Page 1 = 0-9
-   * Page 2 = 9-18
-   * Page 3 = 18-27
-   */
+  return sortedData;
 };
 
 export const contentPagination = (page, limit, contentData) => {
@@ -167,10 +303,10 @@ export const getJobDescsRandomise = [...jobs]
   .map((job) => {
     let more_info = getDetails(job);
     return {
-          jobType: job.job_type
-      .split("-")
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(" "),
+      jobType: job.job_type
+        .split("-")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" "),
       company_name: more_info.company_name,
       job_type: more_info.job_type,
       salary_min: more_info.salary_min,
@@ -179,12 +315,59 @@ export const getJobDescsRandomise = [...jobs]
       title: job.title,
       job_location: job.location ? job.location : more_info.job_location,
       date_posted: job.posted_date,
-       experience: job.experience,
+      experience: job.experience,
     };
   });
 
 let getJobsInRandomOrder = [...jobs].sort(() => Math.random() - 0.5);
 
+
+export const checkJobDescriptionSlug = (job_slug) =>{
+return jobs.findIndex((job) => job.slug === job_slug);
+
+}
+
 export const getJobDescByID = (job_id) => {
-  return jobs.find((job) => job.id === Number(job_id));
+  
+  const job  = jobs.find((job) => job.id === Number(job_id))
+
+  const companyDetails = companies.find((company) => company.id === Number(job.company_id))
+      let more_info = getDetails(job);
+      
+      return {...job,
+        company_name: more_info.company_name,
+        companyDetails:companyDetails,
+        jobType: more_info.jobType,
+        job_type: job.job_type,
+        salary_min: more_info.salary_min,
+        salary_max: more_info.salary_max,
+        url: `/job/${job.id}/${job.slug}`,
+        title: job.title,
+        job_location: job.location ? job.location : more_info.job_location,
+        date_posted: job.posted_date,
+        experience: more_info.experience,
+      };
+
+};
+
+export const getJobsFromSameCategory = (job) => {
+  return jobs
+    .filter((job) => job.category_id === job.category_id)
+    .map((job) => {
+      let more_info = getDetails(job);
+      return {
+        id: job.id,
+        company_name: more_info.company_name,
+        jobType: more_info.jobType,
+        job_type: job.job_type,
+        salary_min: more_info.salary_min,
+        salary_max: more_info.salary_max,
+        url: `/job/${job.id}/${job.slug}`,
+        title: job.title,
+        job_location: job.location ? job.location : more_info.job_location,
+        date_posted: job.posted_date,
+        experience: more_info.experience,
+      };
+    })
+    .slice(0, 3);
 };
