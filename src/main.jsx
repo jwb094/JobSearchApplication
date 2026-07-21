e79e72a899d4bca23ca3f92fbb3e06fa44bcc6ae
Josh Bello <Jb230913@live.com>
2026-07-21 17:48:09 +0100

    update - Add JobsProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
 import { FiltersProvider } from "./context/Filters.jsx";
+import { JobsProvider } from "./context/Jobs.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+    <JobsProvider>
       <FiltersProvider>
         <App />
       </FiltersProvider>
+      </JobsProvider>
     </BrowserRouter>
   </StrictMode>,
 );
