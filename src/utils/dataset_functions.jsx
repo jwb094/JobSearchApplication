5a4e89119d933a92be50343956c9f4869c27a602
Josh Bello <Jb230913@live.com>
2026-07-21 11:04:03 +0100

    update -  logic edit/ added. to these functions checkCompanyExists getCompanyDetailsAndJobs getCompanyDetails getCompanyJobs

--- src/utils/dataset_functions.jsx
+++ src/utils/dataset_functions.jsx
@@ -323,15 +323,57 @@ let getJobsInRandomOrder = [...jobs].sort(() => Math.random() - 0.5);
 
 
 export const checkJobDescriptionSlug = (job_slug) =>{
-return jobs.findIndex((job) => job.slug === job_slug);
+ return jobs.findIndex((job) => job.slug === job_slug);
+}
 
+export const checkCompanyExists = (company_id) =>{
+ return companies.findIndex((company) => company.id === Number(company_id));
+
+}
+
+export const getCompanyDetailsAndJobs =(company_id)=>{
+  const company = companies.find((company) => company.id === Number(company_id));
+
+  const companyJobs = getCompanyJobs(company_id);
+
+  return {
+    company,
+    companyJobs
+  }
+}
+
+export const getCompanyDetails =(company_id)=>{
+return companies.find((company) => company.id === Number(company_id));
+
+}
+
+export const getCompanyJobs = (company_id)=>{
+
+  return jobs.filter((job) => job.company_id === Number(company_id))
+  .map((job)=>{
+    let more_info = getDetails(job);
+      return {
+        id: job.id,
+        company_name: more_info.company_name,
+        jobType: more_info.jobType,
+        job_type: job.job_type,
+        salary_min: more_info.salary_min,
+        salary_max: more_info.salary_max,
+        url: `/job/${job.id}/${job.slug}`,
+        title: job.title,
+        job_location: job.location ? job.location : more_info.job_location,
+        date_posted: job.posted_date,
+        experience: more_info.experience,
+      };
+  })
 }
 
 export const getJobDescByID = (job_id) => {
   
   const job  = jobs.find((job) => job.id === Number(job_id))
 
-  const companyDetails = companies.find((company) => company.id === Number(job.company_id))
+  const companyDetails = getCompanyDetails(job.company_id);
+  //companies.find((company) => company.id === Number(job.company_id))
       let more_info = getDetails(job);
       
       return {...job,
