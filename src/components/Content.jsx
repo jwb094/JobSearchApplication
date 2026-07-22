b0801a3f9c349f7d9b30a35cd4ed2e9e4819a265
Josh Bello <Jb230913@live.com>
2026-07-22 10:02:19 +0100

    update - added utility classes for spacing and layout

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,24 +3,26 @@ import React from "react";
 function Content(props) {
   return (
     <section data-template="Content" data-view="default" className="Content">
-      <h2>{props.title}</h2>
-      <h4>{props.subTitle}</h4>
-      <p>{props.content}</p>
+      <div className="flex flex-col gap-y-6">
+      <h2 className="text-2xl">{props.title}</h2>
+      <h4 className="text-xl">{props.subTitle}</h4>
+      <p className="text-lg">{props.content}</p>
+      </div>
       {/* If contentList has stats */}
       {props.contentList &&
         props.contentList.map((item) => (
-          <div key={item.id}>
-            <h6>{item.title}</h6>
-            <p>{item.content}</p>
+          <div key={item.id} className="flex flex-col gap-y-6 mb-6">
+            <h6 className="text-lg font-semibold">{item.title}</h6>
+            <p className="text-md">{item.content}</p>
           </div>
         ))}
       {/* If content has stats */}
       {props.stats &&
         props.stats.map((item) => (
-          <div key={item.id}>
-            <h6>{item.title}</h6>
-            <p>{item.value}</p>
-            <p>{item.content}</p>
+          <div key={item.id} className="flex flex-col gap-y-4 mb-6">
+            <h6 className="text-lg">{item.title}</h6>
+            <p className="text-xl">{item.value}</p>
+            <p className="text-md">{item.content}</p>
           </div>
         ))}
     </section>
